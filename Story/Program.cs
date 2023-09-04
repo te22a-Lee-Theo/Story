89ef7a7bd98491e141f2d4c8b85e24ddf38bcbbd
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-09-04 10:44:04 +0200

    test if, else if

--- Story/Program.cs
+++ Story/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Vad heter du");
+string name = Console.ReadLine().ToLower();
+
+
+if (name == "theo" || name == "Theo")
+{
+ Console.WriteLine("Välkommen");
+}
+else if (name == "olivia")
+{
+    Console.WriteLine("Hej Hej :)");
+}
+else
+{
+    Console.WriteLine("Hej då :(");
+}
+
+
+// if (name != "theo")
+// {
+    // Console.WriteLine("FUCK OFF");
+// }
+
+Console.ReadLine();
