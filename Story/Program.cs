95d6fe5745cfcc136ac3e86f212a80a7ee344440
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-09-06 10:22:25 +0200

    story version 1

--- Story/Program.cs
+++ Story/Program.cs
@@ -1,25 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Vad heter du");
-string name = Console.ReadLine().ToLower();
+Console.WriteLine("You walk a path in a dark forest and you encounter a fork in the road. Do take the right or the left path?");
+string dir = Console.ReadLine().ToLower();
 
 
-if (name == "theo" || name == "Theo")
+if (dir == "right")
 {
- Console.WriteLine("Välkommen");
+ Console.WriteLine("You continue walking and suddenly a house appears. Do you enter the house?");
 }
-else if (name == "olivia")
+else if (dir == "left")
 {
-    Console.WriteLine("Hej Hej :)");
+    Console.WriteLine("You countinue on the dark path and a branch suddenly breaks and crushes you. GAME OVER");
 }
 else
 {
-    Console.WriteLine("Hej då :(");
+    Console.WriteLine("Please choose a valid direction.");
 }
 
 
-// if (name != "theo")
-// {
-    // Console.WriteLine("FUCK OFF");
-// }
 
 Console.ReadLine();
