927dfc382061707720ecc1963b13e6c6bfd6376c
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-09-06 11:00:29 +0200

    story innan fel fix

--- Story/Program.cs
+++ Story/Program.cs
@@ -3,13 +3,30 @@ Console.WriteLine("You walk a path in a dark forest and you encounter a fork in 
 string dir = Console.ReadLine().ToLower();
 
 
-if (dir == "right")
+if (dir == "right" || dir =="r")
 {
- Console.WriteLine("You continue walking and suddenly a house appears. Do you enter the house?");
+    Console.WriteLine("You continue walking and suddenly a house appears. Do you enter the house?. Yes or no");
+    string wasd = Console.ReadLine().ToLower();
+
+    if (wasd == "yes" || wasd == "y");
+    {
+    Console.WriteLine("You open the dorr to the house, you get struck in the head from behind and gets captured.\nGAME OVER");
+    }
+
+    else if (wasd == "no" || wasd == "n");
+    {
+    Console.WriteLine("You follow the dark path and suddenly you see the light trough the trees and you arrive to your destination.");
+    Console.WriteLine("YOU WIN");
+    }
+
+    else
+    {
+        Console.WriteLine("");
+    }
 }
-else if (dir == "left")
+else if (dir == "left" || dir == "l")
 {
-    Console.WriteLine("You countinue on the dark path and a branch suddenly breaks and crushes you. GAME OVER");
+    Console.WriteLine("You countinue on the dark path. A tree branch suddenly breaks and crushes you. GAME OVER");
 }
 else
 {
